0ce484aafd3ad48e508da1637d2b79a11bdd754f
Fxke <fxkemoney@gmail.com>
2026-06-02 23:15:37 -0700

    Add files via upload

---
+++ Bloxstrap/Models/CustomIntegration.cs
@@ -0,0 +1,10 @@
+﻿namespace Bloxstrap.Models
+{
+    public class RobloxScript
+    {
+        public string Name { get; set; } = "";
+
+        public string Script { get; set; } = "-- Nova Script";
+        public int Delay { get; set; } = 0;
+    }
+}
---
+++ Bloxstrap/Models/Executor.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Bloxstrap.Models
+{
+    public class Executor : ObservableObject
+    {
+        public string Name { get; set; } = "";
+
+        private bool _enabled;
+        public bool Enabled
+        {
+            get => _enabled;
+            set => SetProperty(ref _enabled, value);
+        }
+
+        private string _folder = "";
+        public string Folder
+        {
+            get => _folder;
+            set => SetProperty(ref _folder, value);
+        }
+    }
+}
